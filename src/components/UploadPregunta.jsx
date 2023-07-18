950d435c6ec16640da522c4ae9834373b701364c
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2023-07-18 14:56:07 -0400

    fix: Subir una pregunta a la bd

--- src/components/UploadPregunta.jsx
+++ src/components/UploadPregunta.jsx
@@ -5,6 +5,7 @@ import { Row,Form, Button } from "react-bootstrap";
 import swal from "sweetalert2";
 
 export default function UploadPreguntaComponent() {
+    
     const [pregunta, setPregunta] = useState({
         enunciado: "",
         codigo: "",
@@ -12,110 +13,129 @@ export default function UploadPreguntaComponent() {
         dificultad: "",
     });
 
+
     const handleChange = (e) => {
         setPregunta({
             ...pregunta,
             [e.target.name]: e.target.value,
         });
     };
-    const handleSubmit = () => {
-        swal({
-            title: "¿Estás seguro que deseas subir esta pregunta?",
-            text: "Una vez subida, no podrás modificarla",
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (pregunta.dificultad === "") {
+            swal.fire({
+                title: "Error",
+                text: "Selecciona una dificultad",
+                icon: "error",
+                confirmButtonText: "Aceptar",
+                timer: 2000,
+            });
+            return;
+        }
+        setPregunta({
+            ...pregunta,
+            [e.target.name]: e.target.value,
+        });
+        swal.fire({
+            title: "¨¿Desear subir esta pregunta?",
+            text: "Se subirá la pregunta al banco de preguntas",
             icon: "warning",
-            buttons: ["Cancelar", "Subir"],
-        }).then(respuesta => {
-            if (respuesta) {
-                axios.post("http://localhost:3000/preguntas", pregunta)
-                swal({title: "Pregunta subida",
-                    text: "La pregunta se ha subido correctamente",
-                    icon:"success",
-                    timer: "2000"
+            showCancelButton: true,
+            confirmButtonText: "Subir",
+            cancelButtonText: "Cancelar",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.post("http://localhost:8080/pregunta", pregunta)
+                swal.fire({
+                    title: "Pregunta subida",
+                    text: "La pregunta ha sido subida al banco de preguntas",
+                    icon: "success",
+                    confirmButtonText: "Aceptar",
+                    timer: 2000,
+                }).then((result) => {
+                    window.location.href = "/";
                 });
-                console.log(pregunta);
-                console.log(respuesta.data);
-                console.log(respuesta);
-                window.location.href = "/";
             }
-        })
-    }
+        });
+    };
+
     return (
         <>
             <div>
-            <br />
+                <br />
                 <Row>
                     <h1 className="text-center">Subir Datos del Laboratorio</h1>
                 </Row>
                 <br />
                 <Form onSubmit={handleSubmit}>
                     <Form.Group controlId="enunciado">
-                        <Form.Label>Enunciado:</Form.Label>
-                        <Form.Control
-                            type="text"
-                            value={pregunta.enunciado}
-                            onChange={handleChange}
-                            placeholder="Ingresa el enunciado"
-                            required
-                        />
+                    <Form.Label>Enunciado:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={pregunta.enunciado}
+                        onChange={handleChange}
+                        name="enunciado"
+                        placeholder="Ingresa el enunciado"
+                        required
+                    />
                     </Form.Group>
-
+        
                     <Form.Group controlId="codigo">
-                        <Form.Label>Código:</Form.Label>
-                        <Form.Control
-                            type="text"
-                            value={pregunta.codigo}
-                            onChange={handleChange}
-                            placeholder="Ingresa el código"
-                            required
-                        />
+                    <Form.Label>Código:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={pregunta.codigo}
+                        onChange={handleChange}
+                        name="codigo"
+                        placeholder="Ingresa el código"
+                        required
+                    />
                     </Form.Group>
-
+        
                     <Form.Group controlId="respuesta">
-                        <Form.Label>Respuesta correcta a la pregunta:</Form.Label>
-                        <Form.Control
-                            type="text"
-                            value={pregunta.respuesta}
-                            onChange={handleChange}
-                            placeholder="Ingresa el enunciado"
-                            required
-                        />
+                    <Form.Label>Respuesta correcta a la pregunta:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={pregunta.respuesta}
+                        onChange={handleChange}
+                        name="respuesta"
+                        placeholder="Ingresa la respuesta"
+                        required
+                    />
                     </Form.Group>
-
+        
                     <Form.Group controlId="dificultad">
-                        <Form.Label>Dificultad de la pregunta:</Form.Label>
-                        <div>
+                    <Form.Label>Dificultad de la pregunta:</Form.Label>
+                    <div>
                         <Form.Check
-                            inline
-                            type="radio"
-                            label="Básico"
-                            name="dificultad"
-                            value={0}
-                            checked={pregunta.dificultad === 0}
-                            onChange={handleChange}
+                        inline
+                        type="radio"
+                        label="Básico"
+                        name="dificultad"
+                        value={0}
+                        onChange={handleChange}
                         />
                         <Form.Check
-                            inline
-                            type="radio"
-                            label="Intermedio"
-                            name="dificultad"
-                            value={1}
-                            checked={pregunta.dificultad === 1}
-                            onChange={handleChange}
+                        inline
+                        type="radio"
+                        label="Intermedio"
+                        name="dificultad"
+                        value={1}
+                        onChange={handleChange}
                         />
                         <Form.Check
-                            inline
-                            type="radio"
-                            label="Avanzado"
-                            name="dificultad"
-                            value={2}
-                            checked={pregunta.dificultad === 2}
-                            onChange={handleChange}
+                        inline
+                        type="radio"
+                        label="Avanzado"
+                        name="dificultad"
+                        value={2}
+                        onChange={handleChange}
                         />
-                        </div>
+                    </div>
                     </Form.Group>
-
+        
                     <Button variant="primary" type="submit">
-                        Enviar
+                    Enviar
                     </Button>
                 </Form>
             </div>
